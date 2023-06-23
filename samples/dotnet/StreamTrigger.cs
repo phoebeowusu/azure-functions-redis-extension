fc28bb49fd9601bf07e73d0734b117106ff371ec
Phoebe Owusu <t-powusu@microsoft.com>
2023-06-23 09:51:03 -0700

    Function template for synchronising CosmosDB and Redis using StreamTrigger

--- samples/dotnet/StreamTrigger.cs
+++ samples/dotnet/StreamTrigger.cs
@@ -1,62 +1,58 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Threading.Tasks;
-using Microsoft.Azure.Cosmos;
-using System.Linq;
 using System.Collections.Generic;
-using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 
 namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
-{ 
-    public static class StreamTrigger
+{
+    internal class StreamTrigger
     {
-        // New instance of CosmosClient class using a connection string
         private static CosmosClient client = new(
             connectionString: Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING")!
         );
         public const string localhostSetting = "REDIS_CONNECTION";
-        
+
 
         [FunctionName(nameof(StreamTriggerAsync))]
-            public static async Task StreamTriggerAsync(
+        public static async Task StreamTriggerAsync(
                 [RedisStreamTrigger(localhostSetting, "streamTest")] string entry,
                 ILogger logger)
-            {
-                // Parsing message
-                char[] delimiterChars = { '"', ',', '{', ':', '}' };
-                string[] words = entry.Split(delimiterChars);
-                words = words.Where(x => !string.IsNullOrEmpty(x)).ToArray();
-                string[] vals = new string[words.Length - 3];
-                Array.Copy(words, 3, vals, 0, vals.Length);
+        {
+            //parsing information
+            char[] delimiterChars = { '"', ',', '{', ':', '}' };
+            string[] words = entry.Split(delimiterChars);
+            words = words.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            string[] vals = new string[words.Length - 3];
+            Array.Copy(words, 3, vals, 0, vals.Length);
 
-                // Retrivew CosmosDB database and container  
-                Cosmos.Database database = await client.CreateDatabaseIfNotExistsAsync(
-                    id: "database-id"
-                );
+            //retreiving the database and container
+            Cosmos.Database database = await client.CreateDatabaseIfNotExistsAsync(
+                id: "database-id"
+            );
 
-                Cosmos.Container container = await database.CreateContainerIfNotExistsAsync(
-                    id: "container-id",
-                    partitionKeyPath: "/category",
-                    throughput: 400
-                );
-                
-                // Create new item using message information
-                CosmosItem item = new(
-                    id: words[1],
-                    values: vals
-                );
-                
-                //Unpload item into CosmosDB container
-                CosmosItem upsertedItem = await container.UpsertItemAsync<CosmosItem>(item);
+            Cosmos.Container container = await database.CreateContainerIfNotExistsAsync(
+                id: "container-id",
+                partitionKeyPath: "/category",
+                throughput: 400
+            );
 
-            }     
+            //formatting information
+            CosmosItem item = new(
+                id: words[1],
+                values: vals
+            );
+
+            //upload information into cosmos db
+            CosmosItem upsertedItem = await container.UpsertItemAsync<CosmosItem>(item);
+        }
     }
 }
 
-// C# record type for items in the container
 public record CosmosItem(
     string id,
-    string [] values
+    string[] values
 );
-
