06f53a23ce023d66277b6490cb4f9df73c094df3
Phoebe Owusu <t-powusu@microsoft.com>
2023-06-27 13:34:25 -0700

    Changed CosmosItem class

--- samples/dotnet/StreamTrigger.cs
+++ samples/dotnet/StreamTrigger.cs
@@ -14,25 +14,42 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
     public static class StreamTrigger
     {
         public const string localhostSetting = "REDIS_CONNECTION";
+        public static Dictionary<string, string> ParseResult(StreamEntry entry) => entry.Values.ToDictionary(x => x.Name.ToString(), x => x.Value.ToString());
 
-        [FunctionName(nameof(StreamTriggerAsync))]
-        public static async Task StreamTriggerAsync(
+        [FunctionName(nameof(WriteBehindAsync))]
+        public static async Task WriteBehindAsync(
                 [RedisStreamTrigger(localhostSetting, "streamTest")] StreamEntry entry,
                 [CosmosDB(
                     databaseName: "database-id",
                     containerName: "container-id",
                     Connection = "COSMOS_CONNECTION")]
-                    IAsyncCollector<CosmosItem2> items,
+                    IAsyncCollector<CosmosItem> items,
                 ILogger logger)
         {
-            CosmosItem2 sample = new CosmosItem2 { id = entry.Id, Name = entry.Values[0].ToString() };
-            await items.AddAsync(sample);
+            Dictionary<string, string> dict = ParseResult(entry);
+            CosmosItem sampleItem = new CosmosItem { id = entry.Id, values = dict };
+            await items.AddAsync(sampleItem);
+        }
+
+        [FunctionName(nameof(WriteThrough))]
+        public static void WriteThrough(
+             [RedisStreamTrigger(localhostSetting, "streamTest")] StreamEntry entry,
+                [CosmosDB(
+                    databaseName: "database-id",
+                    containerName: "container-id",
+                    Connection = "COSMOS_CONNECTION")]
+                    ICollector<CosmosItem> items,
+                ILogger logger)
+        {
+            Dictionary<string, string> dict = ParseResult(entry);
+            CosmosItem sampleItem = new CosmosItem { id = entry.Id, values = dict };
+            items.Add(sampleItem);
+
         }
     }
 }
-
-public class CosmosItem2
+public class CosmosItem
 {
     public string id { get; set; }
-    public string Name { get; set; }
+    public Dictionary<string, string> values { get; set; }
 }
