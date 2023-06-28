4e51b02fa5a6c388af2ba2d7dd5b30b3849e00d1
Phoebe Owusu <t-powusu@microsoft.com>
2023-06-28 11:08:01 -0700

    Added Read through function and made changes to write behind and write through functions

--- samples/dotnet/StreamTrigger.cs
+++ samples/dotnet/StreamTrigger.cs
@@ -13,42 +13,105 @@ namespace Microsoft.Azure.WebJobs.Extensions.Redis.Samples
 {
     public static class StreamTrigger
     {
+
         public const string localhostSetting = "REDIS_CONNECTION";
+        public static ConnectionMultiplexer _redisConnection = ConnectionMultiplexer.Connect("REDIS_CONNECTION");
+
+        // Parser helper function for reading results
         public static Dictionary<string, string> ParseResult(StreamEntry entry) => entry.Values.ToDictionary(x => x.Name.ToString(), x => x.Value.ToString());
 
+        // Write behind
         [FunctionName(nameof(WriteBehindAsync))]
         public static async Task WriteBehindAsync(
-                [RedisStreamTrigger(localhostSetting, "streamTest")] StreamEntry entry,
+                [RedisStreamTrigger(localhostSetting, "cosmosWrite")] StreamEntry entry,
                 [CosmosDB(
                     databaseName: "database-id",
                     containerName: "container-id",
                     Connection = "COSMOS_CONNECTION")]
-                    IAsyncCollector<CosmosItem> items,
+                    IAsyncCollector<Data> items,
                 ILogger logger)
         {
+            // Read from Redis writing stream
+            logger.LogInformation("ID: {entry.Id.ToString()}");
+
+            // Map each key value pair
             Dictionary<string, string> dict = ParseResult(entry);
-            CosmosItem sampleItem = new CosmosItem { id = entry.Id, values = dict };
+            foreach (KeyValuePair<string, string> item in dict)
+            {
+                logger.LogInformation(item.Key + "=>" + item.Value.ToString());
+            }
+
+            // Insert into CosmosDB asynchronously
+            Data sampleItem = new Data { id = entry.Id, values = dict };
             await items.AddAsync(sampleItem);
+
+            // Insert into Redis reading stream asynchronously
+            var db = _redisConnection.GetDatabase();
+            await db.StreamAddAsync("cosmosRead", entry.Values, maxLength: 100);
         }
 
+        // Write through
         [FunctionName(nameof(WriteThrough))]
         public static void WriteThrough(
-             [RedisStreamTrigger(localhostSetting, "streamTest")] StreamEntry entry,
+             [RedisStreamTrigger(localhostSetting, "cosmosWrite")] StreamEntry entry,
                 [CosmosDB(
                     databaseName: "database-id",
                     containerName: "container-id",
                     Connection = "COSMOS_CONNECTION")]
-                    ICollector<CosmosItem> items,
+                    ICollector<Data> items,
                 ILogger logger)
         {
+            // Read from Redis writing stream
+            logger.LogInformation("ID: {entry.Id.ToString()}");
+
+            // Map each key value pair
             Dictionary<string, string> dict = ParseResult(entry);
-            CosmosItem sampleItem = new CosmosItem { id = entry.Id, values = dict };
+            foreach (KeyValuePair<string, string> item in dict)
+            {
+                logger.LogInformation(item.Key + "=>" + item.Value.ToString());
+            }
+
+            // Insert into CosmosDB synchronously
+            Data sampleItem = new Data { id = entry.Id, values = dict };
             items.Add(sampleItem);
 
+            // Insert into Redis reading stream synchronously
+            var db = _redisConnection.GetDatabase();
+            db.StreamAdd("cosmosRead", entry.Values, maxLength: 100);
         }
+
+
+        // Read through - WIP
+        [FunctionName(nameof(ReadThrough))]
+        public static void ReadThrough(
+                [RedisStreamTrigger(localhostSetting, "cosmosRead")] StreamEntry entry,
+                [CosmosDB(
+                    databaseName: "database-id",
+                    containerName: "container-id",
+                    Connection = "COSMOS_CONNECTION")]
+                    IReadOnlyList<Data> items,
+                ILogger logger)
+        {
+            // Search Redis reading stream for value
+            var db = _redisConnection.GetDatabase();
+            var messages = db.StreamRead("cosmosRead", "");
+
+            // Cache miss
+            if (messages == null)
+            {
+
+                // Not in CosmosDB 
+                if (items.) return;
+
+                // Insert information from CosmosDB to Redis 
+                db.StreamAdd("cosmosRead", "", maxLength: 100);
+            }
+        }
+
     }
 }
-public class CosmosItem
+
+public class Data
 {
     public string id { get; set; }
     public Dictionary<string, string> values { get; set; }
